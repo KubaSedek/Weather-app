3d5077dbc3359f08d34811e533489b3ec96518fb
Kuba Sędek <contact.kuba@gmail.com>
2017-03-16 17:08:47 +0100

    second and final commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 $(function() {
 	var body = $("body");
-	console.log(body);
-	body.hide();
-	body.fadeIn(1000);
+	var headerHeader = $("body").find("h1");
+
+	function fadeIn(){
+		body.hide();
+		body.fadeIn(1000);
+	}
+	fadeIn();
+
+	function headerFadeIn(){
+		headerHeader.hide();
+		headerHeader.delay(1000).fadeIn(1500);
+	}
+	headerFadeIn();
+
+	function headerScroll() {
+		headerHeader.click(function() {
+		    $('html, body').animate({
+		        scrollTop: $("#section").offset().top
+		    }, 1000);
+		});
+	}
+	headerScroll();
 });
